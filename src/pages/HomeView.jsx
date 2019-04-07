f6314520d44596333e57491def8244c5fd2f2a3d
meows <meow@iceland>
2019-04-07 08:52:22 -0700

    added images

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -7,8 +7,7 @@ import Picture from '../images/example.png'
 const HomeView = () => (
    <section>
       <h1>Hi There! Welcome to my website. Here you can check out about me, the 
-         sports I like, and my school. 
-      </h1>
+         sports I like, and my school.</h1>
       <div className='Button'>Click here to see my pictures</div>
       <img id="BottomImage" src={Picture} />
    </section>
