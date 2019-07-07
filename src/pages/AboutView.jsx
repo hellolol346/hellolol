bc748b3b954cbfa53a33369b40fc15c3e1b53513
Kartik Balu <kartikbalu@Kartiks-MacBook-Pro.local>
2019-07-07 09:37:28 -0700

    fixed typo on quadratic

--- src/pages/AboutView.jsx
+++ src/pages/AboutView.jsx
@@ -19,17 +19,28 @@ const Widget = () => {
 }
 
 const FormWidget = () => {
-   const [firstName, setFirstName] = useState('');
-   const [lastName, setLastName] = useState('');
+   const [a, seta] = useState('');
+   const [b, setb] = useState('');
+   const [c, setc] = useState('');
+   const vertex = -b/ 2*a
+   const rootone = (-b + ((b**2 - 4*a*c)**0.5)) / (2*a)
+   const roottwo = (-b - ((b**2 - 4*a*c)**0.5)) / (2*a)
 
    return (
       <section>
-         <h1>{firstName}</h1>
-         <h1>{lastName}</h1>
+         <h1>{a}</h1>
+         <h1>{b}</h1>
+         <h1>{c}</h1>
          <form>
-            <input value={firstName} onChange={(event) => setFirstName(event.target.value)}></input>
-            <input value={lastName} onChange={(e) => setLastName(e.target.value) }></input>
-         </form>
+            <input value={a} onChange={(event) => seta(event.target.value)}></input>
+            <input value={b} onChange={(e) => setb(e.target.value) }></input>
+            <input value={c} onChange={(e) => setc(e.target.value) }></input>
+            </form>
+            <h1>If you type a negetive number in the place of "a" then the parabola is facing down. 
+               Otherwise, it is facing up</h1>
+               <p> The vertex= {vertex}</p>
+               <p> The first root= {rootone}</p>
+               <p> The second root= {roottwo}</p>
       </section>
    )
 }
