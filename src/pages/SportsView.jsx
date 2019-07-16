139a2a0377976af7ff43054e8b9e0abc35fcec75
Kartik Balu <kartikbalu@Kartiks-MacBook-Pro.local>
2019-07-16 10:29:31 -0700

    Update with W in Class

--- src/pages/SportsView.jsx
+++ src/pages/SportsView.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SportsImage from '../images/soccer2018.jpg'
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Component
+
+const blogPosts = [
+   {
+      title: "This is a title.",
+      date: "June 23, 2020",
+      text: "Blah blah blah blah."
+   },
+   {
+      title: "This is a title.",
+      date: "June 23, 2020",
+      text: "Blah blah blah blah."
+   },
+   {
+      title: "This is a title.",
+      date: "June 23, 2020",
+      text: "Blah blah blah blah."
+   }
+]
+
+function BooleanWidget() {
+   const [flip, setflip]= useState(true);
+   const example = () => {
+      if (flip) {
+         return "Yay."
+      }
+      else {
+         return "No."
+      }
+   }
+
+   return (
+      <section>
+         <h1>State: {String(flip)}</h1>
+         <p> 
+            {
+               // whatever value JavaScript returns will end up here, but only
+               // values may end up here.
+               example()
+            }
+         </p>
+         <p>Another example: {Math.sin(0.5 * Math.PI)}</p>
+         <p>
+            {
+               // this can also be abbreviated:
+               flip ? null : "Puppy Chow is delicious."
+            }
+         </p>
+         <button onClick={() => setflip(!flip)}>flip</button>
+      </section>
+   )
+}
 
 const SportsView = () => (
    <section id="SportsView">
@@ -10,6 +61,16 @@ const SportsView = () => (
       <p>This is my sports page. Here you can see about my soccer team, and you learn    about the sports  I play in school. Meow.</p>
       <div className='Button'>Click here to see my pictures</div>
       <img id="SportsImage" src={SportsImage} />
+      {
+         blogPosts.map(post => (
+            <section>
+               <h1>{post.title}</h1>
+               <p>{post.text}</p>
+            </section>
+         ))
+      }
+
+      <BooleanWidget />
    </section>
 )
 
